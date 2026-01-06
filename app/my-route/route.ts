e4ff1170e2e1893b16eeb9fc79852eecf7140307
Sambid <sambidshk10@gmail.com>
2026-01-06 20:11:59 +0545

    payload integration

--- app/my-route/route.ts
+++ app/my-route/route.ts
@@ -5,7 +5,9 @@ export const GET = async (request: Request) => {
   const payload = await getPayload({
     config: configPromise,
   })
-
+  const data= await payload.find({
+    collection:"categories",
+  })
   return Response.json({
     message: 'This is an example of a custom route.',
   })
