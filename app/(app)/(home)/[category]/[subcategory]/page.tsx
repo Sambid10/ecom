b9ee9e984d473e7f49d2d3f73a6323f39f720c5f
Sambid <sambidshk10@gmail.com>
2026-01-11 17:05:10 +0545

    finished categories features

--- app/(app)/(home)/[category]/[subcategory]/page.tsx
+++ app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -1,15 +1,36 @@
 import ContainerWrapper from '@/components/ui/containerWrapper/ContainerWrapper'
-interface Props{
-    params:Promise<{
-        category:string;
-        subcategory:string
+interface Props {
+    params: Promise<{
+        category: string;
+        subcategory: string
     }>
 }
-export default async function CategoryPage({params}:Props) {
-    const {category,subcategory}=await params
+import Link from 'next/link';
+export default async function CategoryPage({ params }: Props) {
+    const { category, subcategory } = await params
     return (
         <ContainerWrapper>
-            <div className='min-h-screen mt-2'>{category}/{subcategory}</div>
+            <div className='min-h-screen ml-4'>
+                <div className='flex items-center gap-2'>
+                   <Link
+                        className='text-blue-600 font-medium text-base capitalize  hover:underline'
+                        href={`/`}
+                    >Home</Link>
+                       <h1 className='text-blue-500
+                00'>&gt;</h1>
+                    <Link
+                        className='text-blue-600 font-medium text-base capitalize  hover:underline'
+                        href={`/${category}`}
+                    >{category}</Link>
+                    <h1 className='text-blue-500
+                00'>&gt;</h1>
+                    <Link
+                        className='text-blue-600 font-medium text-base capitalize  hover:underline'
+                        href={`/${category}/${subcategory}`}
+                    >{subcategory}</Link>
+                </div>
+
+            </div>
         </ContainerWrapper>
 
     )
--- app/(app)/(home)/[category]/page.tsx
+++ app/(app)/(home)/[category]/page.tsx
@@ -1,14 +1,29 @@
 import ContainerWrapper from '@/components/ui/containerWrapper/ContainerWrapper'
-interface Props{
-    params:Promise<{
-        category:string
+interface Props {
+    params: Promise<{
+        category: string
     }>
 }
-export default async function CategoryPage({params}:Props) {
-    const {category}=await params
+import Link from 'next/link'
+export default async function CategoryPage({ params }: Props) {
+    const { category } = await params
     return (
         <ContainerWrapper>
-            <div className='min-h-screen mt-2'>{category}</div>
+            <div className='min-h-screen ml-4'>
+                <div className='flex items-center gap-2'>
+                    <Link
+                        className='text-blue-600 font-medium text-base capitalize  hover:underline'
+                        href={`/`}
+                    >Home</Link>
+                    <h1 className='text-blue-500
+                00'>&gt;</h1>
+                    <Link
+                        className='text-blue-600 font-medium text-base capitalize hover:underline'
+                        href={`${category}`}
+                    >{category}</Link>
+                </div>
+
+            </div>
         </ContainerWrapper>
 
     )
